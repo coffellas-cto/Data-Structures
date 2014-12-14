a210c98c876da5d1a22ff34f90ef1e708c6f5ad6
coffellas-cto <cto@coffellas.com>
2014-12-13 19:16:12 -0800

    MinMax Binary Heap

--- DataStructures/BinaryHeap.swift
+++ DataStructures/BinaryHeap.swift
@@ -2,62 +2,120 @@
 //  BinaryHeap.swift
 //  DataStructures
 //
-//  Created by Alex G on 15.11.14.
+//  Created by Alex G on 13.12.14.
 //  Copyright (c) 2014 Alexey Gordiyenko. All rights reserved.
 //
-
-// TODO: Not finished
 
 import Foundation
 
 class BinaryHeap<T: Comparable>: DebugPrintable {
-    private var array: [T?]!
+    private var array: Array<T?>
     private var used: Int = 0
+    private var comparison: (T, T) -> Bool
     
     // MARK: Private Methods
+    private func swap(#index1: Int, index2: Int) {
+        var tmp = array[index1]
+        array[index1] = array[index2]
+        array[index2] = tmp
+    }
     
-    func grow() {
-        array! += [T?](count: array.count, repeatedValue: nil)
-    } // __CFBinaryHeapGrow
+    private func parentIndex(index: Int) -> Int {
+        return (index - 1) / 2
+    }
     
-    // MARK: Public Methods
+    private func leftChildIndex(index: Int) -> Int {
+        return index * 2 + 1
+    }
     
-    func addValue(newValue: T) {
-        println("Adding: \(newValue)")
-        if (used == array.count) {
-            grow()
+    private func righChildIndex(index: Int) -> Int {
+        return index * 2 + 2
+    }
+    
+    private func grow() {
+        array += [T?](count: array.count, repeatedValue: nil)
+    }
+    
+    private func shrink() {
+        array = Array<T?>(array[0...array.count/2])
+    }
+    
+    private func heapifyToTop(index: Int, comparison: (T, T) -> Bool) {
+        let parentIndex = self.parentIndex(index)
+        
+        if comparison(self.array[index]!, self.array[parentIndex]!) {
+            self.swap(index1: index, index2: parentIndex)
+            
+            if parentIndex != 0 {
+                self.heapifyToTop(parentIndex, comparison)
+            }
+        }
+    }
+    
+    private func heapifyToBottom(index: Int, comparison: (T, T) -> Bool) {
+        let leftChildIndex = self.leftChildIndex(index)
+        let rightChildIndex = self.righChildIndex(index)
+        
+        var selectedChildIndex: Int? = nil
+        if (leftChildIndex > used) && (rightChildIndex <= used) {
+            selectedChildIndex = rightChildIndex
+        } else if (rightChildIndex > used) && (leftChildIndex <= used) {
+            selectedChildIndex = leftChildIndex
+        } else if (rightChildIndex <= used) && (leftChildIndex <= used) {
+            if comparison(self.array[leftChildIndex]!, self.array[rightChildIndex]!) {
+                selectedChildIndex = leftChildIndex
+            } else {
+                selectedChildIndex = rightChildIndex
+            }
         }
         
-        used++
-        
-        var i = used - 1
-        array[i] = newValue;
-        
-        while (i > 0) && (array[i / 2] > array[i]) {
-            var tmp = array[i / 2]
-            array[i / 2] = array[i]
-            array[i] = tmp
-            i /= 2
+        if let selectedChildIndex = selectedChildIndex {
+            if comparison(self.array[selectedChildIndex]!, self.array[index]!) {
+                self.swap(index1: selectedChildIndex, index2: index)
+                self.heapifyToBottom(selectedChildIndex, comparison)
+            }
+        }
+    }
+    
+    // Internal Methods
+    
+    internal func removeTop() -> T? {
+        if used == 0 {
+            return nil
         }
         
-    } // CFBinaryHeapAddValue
+        var retVal = self.array[0]
+        self.array[0] = self.array[used - 1]
+        heapifyToBottom(0, comparison)
+        
+        if used <= self.array.count / 4 {
+            self.shrink()
+        }
+        
+        self.used--
+        return retVal
+    }
     
-    func containsValue(value: T) -> Bool {
-        return false
-    } // CFBinaryHeapContainsValue
+    internal func getTop() -> T? {
+        return self.array[0]
+    }
     
-    func minimum() -> T? {
-        return nil
-    } // CFBinaryHeapGetMinimum
+    // Public Methods:
+    func addValue(value: T) {
+        if used == self.array.count {
+            self.grow()
+        }
+        
+        self.array[used] = value
+        heapifyToTop(used, comparison)
+        self.used++
+    }
     
-    func removeMinimum() {
-    } // CFBinaryHeapRemoveMinimumValue
+    // MARK: Life Cycle
     
-    func clear() {
-    } // CFBinaryHeapRemoveAllValues
-    
-    init() {
-        array = [T?](count: 1, repeatedValue: nil)
+    init(size newSize: Int, comparison newComparison: (T, T) -> Bool) {
+        array = [T?](count: newSize, repeatedValue: nil)
+        comparison = newComparison
     }
     
     // MARK: DebugPrintable Protocol Methods
@@ -85,5 +143,33 @@ class BinaryHeap<T: Comparable>: DebugPrintable {
         }
         retVal += "[/binary_heap]"
         return retVal
+    }
+}
+
+class MaxHeap<T: Comparable>: BinaryHeap<T> {
+    func getMax() -> T? {
+        return self.getTop()
+    }
+    
+    func removeMax() -> T? {
+        return self.removeTop()
+    }
+    
+    init(size newSize: Int) {
+        super.init(size: newSize, {$0 > $1})
+    }
+}
+
+class MinHeap<T: Comparable>: BinaryHeap<T> {
+    func getMin() -> T? {
+        return self.getTop()
+    }
+    
+    func removeMin() -> T? {
+        return self.removeTop()
+    }
+    
+    init(size newSize: Int) {
+        super.init(size: newSize, {$0 <= $1})
     }
 }
